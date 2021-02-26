d4036fa7de3f4713f0efd23a35074b32e20d3251
Rishat Suleymanov <spk_irbis@mail.ru>
2021-02-26 23:36:04 +0300

    Changes in the main class

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -3,11 +3,13 @@ package ru.netology;
 public class Main {
     public static void main(String[] args) {
         ThreadGroup threadGroup = new ThreadGroup("main group");
-        Thread mt1 = new Thread(threadGroup, new MyThread());
-        Thread mt2 = new Thread(threadGroup, new MyThread());
-        Thread mt3 = new Thread(threadGroup, new MyThread());
-        Thread mt4 = new Thread(threadGroup, new MyThread());
 
+        Thread mt1 = new MyThread("1");
+        Thread mt2 = new MyThread("2");
+        Thread mt3 = new MyThread("3");
+        Thread mt4 = new MyThread("4");
+
+        System.out.println("Creating Threads...");
         mt1.start();
         mt2.start();
         mt3.start();
@@ -15,13 +17,13 @@ public class Main {
 
         try {
             Thread.sleep(15000);
+            mt1.interrupt();
+            mt2.interrupt();
+            mt3.interrupt();
+            mt4.interrupt();
+            System.out.println("Terminating all threads.");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        threadGroup.interrupt();
-        System.out.println("Ended all threads.");
-
-
     }
 }
--- src/main/java/ru/netology/MyThread.java
+++ src/main/java/ru/netology/MyThread.java
@@ -2,17 +2,21 @@ package ru.netology;
 
 public class MyThread extends Thread {
 
+    public MyThread(String s) {
+        this.setName(s);
+    }
+
     @Override
     public void run() {
         try {
             while (!isInterrupted()) {
                 Thread.sleep(2500);
-                System.out.printf("I am %s", Thread.currentThread().getName() + ". Hello everyone!\n");
+                System.out.printf("I am Thread %s", Thread.currentThread().getName() + ". Hello everyone!\n");
             }
         } catch (InterruptedException e) {
 
         } finally {
-            System.out.printf("Thread %s ended.\n", Thread.currentThread().getName());
+            System.out.printf("Thread %s ended.\n", getName());
         }
     }
 }
